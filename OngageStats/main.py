b32e533e3dd1c8f52cd50d5660516a719efb74d1
Radu <ionescu.radu.18@gmail.com>
2022-07-20 11:14:17 +0300

    FIX: formats

--- OngageStats/main.py
+++ OngageStats/main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import datetime
 import glob
 import os
@@ -10,7 +11,7 @@ class OngageStatParser(HTMLParser):
         all_data.append(data)
 
 
-def validate(date_text):
+def validate_date(date_text):
     try:
         return datetime.datetime.strptime(date_text, '%b %d, %Y')
     except ValueError:
@@ -52,26 +53,38 @@ for i in campaign_date_indices:
     l = [idx for idx in range(len(all_data[i])) if all_data[i][idx] == '2']
     if f and l:
         c_date = all_data[i][f[0]: l[-1] + 1]
-        if validate(c_date):
-            campaign_dates.append(validate(c_date))
+        if validate_date(c_date):
+            campaign_dates.append(validate_date(c_date))
 for i in subject_indices:
     subjects.append(all_data[i])
 for i in list_name_indices:
-    list_names.append(''.join([c for c in all_data[i] if c.isupper() or c == ' ' or c == '-']).strip())
+    list_names.append(''.join([c for c in all_data[i] if c.isupper() or c == ' ' or c == '-']).strip().title())
 for i in campaign_name_indices:
     campaign_names.append(all_data[i].strip())
 for i in sent_indices:
-    sent.append(all_data[i])
+    sent.append(int(str(all_data[i]).replace(',', '', 1)))
 for i in open_indices:
-    opens.append(all_data[i])
+    opens.append(int(str(all_data[i]).replace(',', '', 1)))
     open_rates.append(all_data[i + 2])
 for i in click_indices:
-    clicks.append(all_data[i])
+    clicks.append(int(str(all_data[i]).replace(',', '', 1)))
     click_rates.append(all_data[i + 2])
 
+campaign_dates_string = []
+for date in campaign_dates:
+    campaign_dates_string.append(date.strftime('%d.%m.%Y'))
+
 for i in range(4):
-    print('Date: {} Subject: {} Open rate: {} Click rate: {} Sent: {} Open: {} Clicks: {} List: {}'.format(
-        campaign_dates[i].strftime('%d.%m.%Y'),
+    if list_names[i] == 'All Lists-Go':
+        list_names[i] = 'Get Openers'
+    if list_names[i] == 'Networks':
+        list_names[i] = 'Network'
+    if list_names[i] == 'Networks Guests':
+        list_names[i] = 'Network Guest'
+
+for i in range(4):
+    print('Date: {} Subject: {} Open rate: {} Click rate: {} Sent: {} Open: {} Clicks: {} List: {}, Campaign: {}'.format(
+        campaign_dates_string[i],
         subjects[i],
         open_rates[i],
         click_rates[i],
@@ -79,4 +92,20 @@ for i in range(4):
         opens[i],
         clicks[i],
         list_names[i],
+        campaign_names[i],
     ))
+
+column_1 = 'Date'
+column_2 = 'List'
+column_3 = 'Subject'
+column_4 = 'Mailer count'
+column_5 = 'Open Rate'
+column_6 = 'CTR'
+column_7 = 'Sent number'
+column_8 = 'Open Number'
+column_9 = 'Clicks Number'
+
+data_frame = pd.DataFrame(
+    {column_1: campaign_dates_string, column_2: list_names, column_3: subjects, column_4: ['', '', '', ''], column_5: open_rates,
+     column_6: click_rates, column_7: sent, column_8: opens, column_9: clicks})
+data_frame.to_excel('/Users/IonescuRadu/Downloads/5.xlsx', sheet_name='sheet1', index=False)
